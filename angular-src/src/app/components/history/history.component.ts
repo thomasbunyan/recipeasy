79715222cb704c6d3242dcd70b0d5ac93b5b4cc7
Thomas Bunyan <thomaspaulbunyan@gmail.com>
2019-01-25 08:17:52 +0000

    Added the history component

--- angular-src/src/app/components/history/history.component.ts
+++ angular-src/src/app/components/history/history.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  selector: "app-history",
+  templateUrl: "./history.component.html",
+  styleUrls: ["./history.component.css"]
 })
 export class HistoryComponent implements OnInit {
+  userData: any;
 
-  constructor() { }
+  constructor(private userService: UserService, private titleService: Title) {}
 
   ngOnInit() {
+    this.titleService.setTitle("History");
+    this.userService.getUserData().subscribe(data => {
+      this.userData = data;
+    });
   }
 
+  viewItem(item) {
+    console.log("View item");
+    // if ((item.type = "recipe")) {
+    //   console.log("Recipe");
+    // } else {
+    //   console.log("Cookbook");
+    // }
+  }
 }
--- api/models/user.js
+++ api/models/user.js
@@ -19,7 +19,7 @@ const UserSchema = mongoose.Schema({
   },
   active: {
     type: Boolean,
-    default: true,
+    default: false,
     required: true
   },
   date: {
